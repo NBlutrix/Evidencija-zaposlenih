df684ba1429c915714a7bccda219eb26cacdec88
Nikola Mandic <nikolamandan@gmail.com>
2025-10-06 20:46:36 +0300

    Dodati modeli, migracije i podešavanje baze

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Attendance extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'date',
+        'arrival_time',
+        'departure_time',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
