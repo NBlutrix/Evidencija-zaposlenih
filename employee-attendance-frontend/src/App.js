cb1243e9e19fb3de5329c106212eec4e2dfffc95
Nikola Mandic <nikolamandan@gmail.com>
2025-10-09 13:39:15 +0300

    App js

--- employee-attendance-frontend/src/App.js
+++ employee-attendance-frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import AttendancePage from './pages/AttendancePage';
+import ExportPage from './pages/ExportPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/attendances" element={<AttendancePage />} />
+        <Route path="/export" element={<ExportPage />} />
+      </Routes>
+    </Router>
   );
 }
 
