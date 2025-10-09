b1f69fc467d5a1f81203fab4bfc5f094727b9bb0
Nikola Mandic <nikolamandan@gmail.com>
2025-10-09 13:35:14 +0300

    Home page

---
+++ employee-attendance-frontend/src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div>
+    <h1>Welcome to Employee Attendance App</h1>
+    <p>Use the navigation bar to manage attendances or export ICS files.</p>
+  </div>
+);
+
+export default HomePage;
